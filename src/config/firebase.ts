bab6ce8604a123a856bb711f104cdd214467e97a
김건우 <gimgeon-u@gimgeon-uui-MacBookAir.local>
2020-03-22 16:36:28 +0900

    개발환경셋팅

---
+++ src/config/firebase.ts
@@ -0,0 +1,9 @@
+export default {
+  apiKey: 'AIzaSyDkgiJNMt6lxKBd50kXCpou9o2HC2k5sNk',
+  authDomain: 'todayidressedup.firebaseapp.com',
+  databaseURL: 'https://todayidressedup.firebaseio.com',
+  projectId: 'todayidressedup',
+  storageBucket: 'todayidressedup.appspot.com',
+  messagingSenderId: '983956591494',
+  appId: '1:983956591494:web:8d979b1f61583f8bdfd207',
+};
